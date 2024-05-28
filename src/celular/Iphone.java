0bb675f08f9bbfbd6f319687891edc7e043791a2
DaniNishikino <nimguem1453@gmail.com>
2024-05-28 12:42:22 -0300

    Fiz uma representação de um celular

---
+++ src/celular/Iphone.java
@@ -0,0 +1,52 @@
+package celular;
+
+import aparelhos.AparelhoTelefonico;
+import aparelhos.NavegadorInternet;
+import aparelhos.ReprodutorMusical;
+
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para o numero " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Chamada atendida");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Correio de voz iniciado");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo pagina");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Pagina atualizada");
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando musica");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Musica pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("A musica " + musica + " foi selecionada");
+    }
+}
